f8a3fb7d7902e09ae4e3fa9a50ba262f6b754646
Stephan Schiffels <stephan.schiffels@mac.com>
2014-10-21 20:38:12 +0100

    m

---
+++ genToVcf.hs
@@ -0,0 +1,20 @@
+import Data.List.Split (chunksOf, splitOn)
+import Data.List (intercalate)
+import Control.Monad (liftM)
+import System.Environment (getArgs)
+
+convertLine :: String -> String -> String
+convertLine chr line =
+    let (_:_:pos:ref:alt:gens) = splitOn " " line
+        genFields = map makeGenField $ chunksOf 3 (map read gens)
+    in  intercalate "\t" $ [chr, pos, ".", ref, alt, "100", ".", ".", "GT:GP"] ++ genFields
+
+makeGenField :: [Double] -> String
+makeGenField [p1, p2, p3] =
+    if p1 > p2 && p1 > p3 then "0/0:" ++ probStr else
+        if p2 > p1 && p2 > p3 then "0/1:" ++ probStr else "1/1:" ++ probStr
+    where probStr = intercalate "," $ map show [p1, p2, p3]
+
+main = do
+    chr <- liftM head getArgs
+    interact $ unlines . map (convertLine chr) . lines
