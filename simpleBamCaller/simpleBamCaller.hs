dc51eae46779cfad3c4f32980b982757d7cb59c2
Stephan Schiffels <schiffels_schiffels@mac.com>
2016-04-11 11:12:54 +0200

    added freqSum header and *.ind.txt output to Eigenstrat

--- simpleBamCaller/simpleBamCaller.hs
+++ simpleBamCaller/simpleBamCaller.hs
@@ -3,54 +3,57 @@
 import OrderedZip (orderedZip)
 
 import Control.Exception.Base (throwIO, AssertionFailed(..))
-import Control.Monad (forM, void)
 import Control.Monad.Random (evalRandIO)
-import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.State.Strict (runStateT)
 import qualified Data.Attoparsec.Text as A
 import Data.Char (isSpace)
-import Data.List (sortBy)
+import Data.List (sortBy, intercalate)
 import Data.Maybe (catMaybes)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Debug.Trace (trace)
+-- import Debug.Trace (trace)
 import qualified Options.Applicative as OP
-import Pipes (Pipe, await, yield, (>->), runEffect, Producer, Pipe, for, cat, next)
-import Pipes.Attoparsec (parsed)
+import Pipes (Pipe, yield, (>->), runEffect, Producer, Pipe, for, cat, next)
+import Pipes.Attoparsec (parsed, parse)
 import Pipes.Cliff (CreateProcess(..), CmdSpec(..), pipeOutput, NonPipe(..))
 import Pipes.Cliff.Core (defaultHandler)
 import qualified Pipes.Prelude as P
-import Pipes.Safe (runSafeT, SafeT, MonadSafe)
+import Pipes.Safe (runSafeT, SafeT)
 import Pipes.Text.Encoding (decodeUtf8)
 import qualified Pipes.Text.IO as PT
 import Prelude hiding (FilePath)
-import System.IO (withFile, IOMode(..))
+import System.IO (withFile, IOMode(..), hPutStrLn)
 import System.Random (randomRIO, mkStdGen, setStdGen)
 import System.Random.Shuffle (shuffleM)
 import Turtle hiding (tab, cat)
 
-data ProgOpt = ProgOpt {
-    optCallingMode :: CallingMode,
-    optSeed :: Maybe Int,
-    optMinDepth :: Int,
-    optTransversionsOnly :: Bool,
-    optSnpFile :: Maybe FilePath,
-    optRegion :: Text,
-    optOutChrom :: Maybe Text,
-    optReference :: FilePath,
-    optOutFormat :: OutFormat,
-    optSnpOutFile :: Maybe FilePath,
-    optSamtoolsExe :: FilePath,
-    optBcftoolsExe :: FilePath,
-    optBamFiles :: [FilePath]
-}
+data ProgOpt = ProgOpt CallingMode (Maybe Int) Int Bool (Maybe FilePath) Text (Maybe Text) 
+                       FilePath OutFormat (Maybe FilePath) FilePath FilePath [FilePath]
+
+-- optCallingMode :: CallingMode,
+-- optSeed :: Maybe Int,
+-- optMinDepth :: Int,
+-- optTransversionsOnly :: Bool,
+-- optSnpFile :: Maybe FilePath,
+-- optRegion :: Text,
+-- optOutChrom :: Maybe Text,
+-- optReference :: FilePath,
+-- optOutFormat :: OutFormat,
+-- optEigenstratOutPrefix :: Maybe FilePath,
+-- optSamtoolsExe :: FilePath,
+-- optBcftoolsExe :: FilePath,
+-- optBamFiles :: [FilePath]
 
 data CallingMode = MajorityCalling | RandomCalling | RareCalling deriving (Show, Read)
 data OutFormat = EigenStrat | FreqSumFormat deriving (Show, Read)
 data FreqSumRow = FreqSumRow Text Int Char Char [Int] deriving (Show)
             -- Chrom Pos Alleles Number_of_reads_per_individual
 data VCFentry = VCFentry Text Int [Char] [[Int]] deriving (Show)
+data VCFheader = VCFheader [Text] [String] -- simple comment lines, sample names
 data SnpEntry = SnpEntry Text Int Char Char deriving (Show)-- Chrom Pos Ref Alt
 
+main :: IO ()
 main = OP.execParser parser >>= runWithOpts
   where
     parser = OP.info (OP.helper <*> argParser)
@@ -59,7 +62,7 @@ main = OP.execParser parser >>= runWithOpts
 argParser :: OP.Parser ProgOpt
 argParser = ProgOpt <$> parseCallingMode <*> parseSeed <*> parseMinDepth <*>
                         parseTransversionsOnly <*> parseSnpFile <*> parseChrom <*>
-                        parseOutChrom <*> parseRef <*> parseFormat <*> parseSnpOutFile <*> 
+                        parseOutChrom <*> parseRef <*> parseFormat <*> parseEigenstratOutPrefix <*> 
                         parseSamtoolsExe <*> parseBcftoolsExe <*> OP.some parseBams
   where
     parseCallingMode = OP.option OP.auto (OP.long "mode" <> OP.short 'm' <>
@@ -109,11 +112,13 @@ argParser = ProgOpt <$> parseCallingMode <*> parseSeed <*> parseMinDepth <*>
                                      OP.short 'o' <>
                                      OP.value FreqSumFormat <> OP.showDefault <>
                                      OP.help "specify output format: EigenStrat or FreqSum")
-    parseSnpOutFile = OP.option (Just . fromText . T.pack <$> OP.str) (OP.long "snpOutFile" <> 
-                                    OP.short 's' <>
-                                 OP.value Nothing <> OP.metavar "<FILE>" <>
-                                 OP.help "specify the EigenStrat SNP file. Ignored if Output \
-                                          \format is not Eigenstrat")
+    parseEigenstratOutPrefix = OP.option (Just . fromText . T.pack <$> OP.str)
+                                 (OP.long "eigenstratOutPrefix" <> 
+                                    OP.short 'e' <>
+                                 OP.value Nothing <> OP.metavar "<FILE_PREFIX>" <>
+                                 OP.help "specify the filenames for the EigenStrat SNP and IND \
+                                 \file outputs: <FILE_PREFIX>.snp.txt and <FILE_PREFIX>.ind.txt \
+                                 \Ignored if Output format is not Eigenstrat")
     parseSamtoolsExe = OP.option (fromText . T.pack <$> OP.str) (OP.long "samtools" <>
                                 OP.value "samtools" <> OP.showDefault <>
                                   OP.metavar "<SAMTOOLS_PATH>" <>
@@ -125,7 +130,7 @@ argParser = ProgOpt <$> parseCallingMode <*> parseSeed <*> parseMinDepth <*>
     
 runWithOpts :: ProgOpt -> IO ()
 runWithOpts (ProgOpt mode seed minDepth transversionsOnly snpFile region outChrom reference 
-                     outFormat snpOutFile samtools bcftools bamFiles) = do
+                     outFormat eigenStratOutPrefix samtools bcftools bamFiles) = do
     case seed of
         Nothing -> return ()
         Just seed_ -> setStdGen $ mkStdGen seed_
@@ -133,38 +138,45 @@ runWithOpts (ProgOpt mode seed minDepth transversionsOnly snpFile region outChro
     let outputChrom = case outChrom of
             Nothing -> chrom
             Just label -> label
-    freqSumProducer <- case snpFile of
+    (vcfHeader, freqSumProducer) <- case snpFile of
         Nothing -> do
             let cmd = format (fp%" mpileup -q30 -Q30 -C50 -I -f "%fp%" -g -t DPR -r "%s%" "%s%
                               " | "%fp%" view -v snps -H") samtools reference region bams bcftools
             vcfTextProd <- produceFromCommand cmd
-            let vcfProd = parsed vcfParser vcfTextProd
-            return $ vcfProd >-> processVcfSimple (length bamFiles) mode minDepth transversionsOnly
+            (vcfHeader_, vcfProd) <- parseVCF vcfTextProd
+            let vcfProdPipe = vcfProd >-> processVcfSimple (length bamFiles) mode minDepth  
+                                          transversionsOnly
+            return (vcfHeader_, vcfProdPipe)
         Just fn -> do
             let cmd = format (fp%" mpileup -q30 -Q30 -C50 -I -f "%fp%" -g -t DPR -r "%s%" -l "%fp%
                               " "%s%" | "%fp%" view -H") samtools reference region fn bams bcftools
             vcfTextProd <- produceFromCommand cmd
             let snpTextProd = PT.readFile ((T.unpack . format fp) fn)
-            let vcfProd = parsed vcfParser vcfTextProd
+            (vcfHeader_, vcfProd) <- parseVCF vcfTextProd
             let snpProd =
                     parsed snpParser snpTextProd >-> P.filter (\(SnpEntry c _ _ _) -> c == chrom)
             let jointProd = orderedZip cmp snpProd vcfProd
-            return . fmap snd $ jointProd >-> processVcfWithSnpFile (length bamFiles) mode minDepth 
-                                              transversionsOnly
-    res <- case outFormat of
-            FreqSumFormat -> runSafeT . runEffect $ freqSumProducer >->
-                                        P.map (printFreqSum outputChrom) >-> printToStdOut
-            EigenStrat -> case snpOutFile of
-                Nothing -> throwIO $ AssertionFailed ("need a snpOutFile for EigenStratFormat")
-                Just fn -> withFile (T.unpack . format fp $ fn) WriteMode $ \snpOutHandle ->
+                jointProdPipe =
+                    jointProd >-> processVcfWithSnpFile (length bamFiles) mode minDepth 
+                                                        transversionsOnly
+            return (vcfHeader_, fmap snd jointProdPipe)
+    case outFormat of
+        FreqSumFormat -> do
+            let VCFheader _ n = vcfHeader
+            putStrLn $ "#CHROM\tPOS\tREF\tALT\t" ++ (intercalate "\t" . map (++"(2)") $ n)
+            runSafeT . runEffect $ freqSumProducer >->
+                         P.map (showFreqSum outputChrom) >-> printToStdOut
+        EigenStrat -> case eigenStratOutPrefix of
+            Nothing -> throwIO $ AssertionFailed "need an eigenstratPrefix for EigenStratFormat"
+            Just fn -> do
+                let snpOut = fn <.> "snp.txt"
+                    indOut = fn <.> "ind.txt"
+                withFile (T.unpack . format fp $ indOut) WriteMode $ \indOutHandle -> do
+                    let VCFheader _ sampleNames = vcfHeader
+                    mapM_ (\n -> hPutStrLn indOutHandle (n ++ "\tU\tUnknown")) sampleNames
+                withFile (T.unpack . format fp $ snpOut) WriteMode $ \snpOutHandle ->
                     runSafeT . runEffect $ freqSumProducer >->
-                                    printEigenStrat outputChrom snpOutHandle >-> printToStdOut
-    case res of
-        Left (e, rest) -> do
-            err . format w $ e
-            Right (c, _) <- runSafeT $ next rest
-            err c
-        Right () -> return ()
+                    printEigenStrat outputChrom snpOutHandle >-> printToStdOut
   where
     bams = (T.intercalate " " (map (format fp) bamFiles))
     cmp (SnpEntry _ snpPos _ _) (VCFentry _ vcfPos _ _) = snpPos `compare` vcfPos
@@ -176,6 +188,35 @@ produceFromCommand cmd = do
                                                         False defaultHandler
     (p, _) <- pipeOutput Inherit Inherit createProcess
     return . void . decodeUtf8 $ p
+
+parseVCF :: Producer Text (SafeT IO) () -> IO (VCFheader, Producer VCFentry (SafeT IO) ())
+parseVCF prod = do
+    (res, rest) <- runSafeT $ runStateT (parse vcfHeaderParser) prod
+    header <- case res of
+        Nothing -> throwIO $ AssertionFailed "vcf file exhausted"
+        Just (Left e_) -> throwIO $ AssertionFailed ("vcf file parsing error: " ++ show e_)
+        Just (Right h) -> return h
+    return (header, parsed vcfParser rest >>= liftErrors)
+  where
+    liftErrors res = case res of
+        Left (e_, prod_) -> do
+            Right (chunk, _) <- lift $ next prod_
+            let msg = show e_ ++ T.unpack chunk
+            lift . liftIO . throwIO $ AssertionFailed msg
+        Right () -> return ()
+
+vcfHeaderParser :: A.Parser VCFheader
+vcfHeaderParser = VCFheader <$> A.many' doubleCommentLine <*> singleCommentLine
+  where
+    doubleCommentLine = do
+        c1 <- A.string "##"
+        s_ <- A.takeTill A.isEndOfLine <* A.endOfLine
+        return $ T.append c1 s_
+    singleCommentLine = do
+        void $ A.char '#'
+        s_ <- A.takeTill A.isEndOfLine <* A.endOfLine
+        let fields = T.splitOn "\t" s_
+        return . drop 9 . map T.unpack $ fields
 
 vcfParser :: A.Parser VCFentry
 vcfParser = do
@@ -200,7 +241,7 @@ vcfParser = do
     altAllele = Just <$> A.satisfy (A.inClass "ACTG")
     xAllele = A.string "<X>" >> return Nothing
     coverage = do
-        _ <- A.decimal `A.sepBy1` (A.char ',')
+        _ <- A.decimal `A.sepBy1` (A.char ',') :: A.Parser [Int]
         _ <- A.char ':'
         A.decimal `A.sepBy1` (A.char ',')
 
@@ -216,7 +257,7 @@ processVcfSimple nrInds mode minDepth transversionsOnly = for cat $ \vcfEntry ->
     when (length covNums /= nrInds) $ (liftIO . throwIO) (AssertionFailed "inconsistent number \
             \of genotypes. Check that bam files have different readgroup sample names")
     (normalizedAlleles, normalizedCovNums) <- case alleles of
-        [ref] -> liftIO . throwIO $ AssertionFailed "should not happen, need at least one \
+        [_] -> liftIO . throwIO $ AssertionFailed "should not happen, need at least one \
                             \alternative allele"
         [ref, alt] -> return ([ref, alt], covNums)
         _ -> do
@@ -245,7 +286,7 @@ callGenotype :: CallingMode -> Int -> [Int] -> IO Int
 callGenotype mode minDepth covs = do
     if sum covs < minDepth then return (-1) else do
         case covs of
-            [numRef] -> return 0
+            [_] -> return 0
             [numRef, numAlt] -> do
                 case mode of
                     MajorityCalling -> case numRef `compare` numAlt of
@@ -270,7 +311,7 @@ processVcfWithSnpFile nrInds mode minDepth transversionsOnly = for cat $ \jointE
         (Just (SnpEntry snpChrom snpPos snpRef snpAlt), Nothing) -> do
             yield $ FreqSumRow snpChrom snpPos snpRef snpAlt (replicate nrInds (-1))
         (Just (SnpEntry snpChrom snpPos snpRef snpAlt),
-         Just (VCFentry vcfChrom vcfPos vcfAlleles vcfNums)) -> do
+         Just (VCFentry _ _ vcfAlleles vcfNums)) -> do
             when (length vcfNums /= nrInds) $ (liftIO . throwIO) (AssertionFailed "inconsistent \ 
                             \number of genotypes. Check that bam files have different \
                             \readgroup sample names")
@@ -308,10 +349,10 @@ snpParser = do
     _ <- A.satisfy (\c -> c == '\r' || c == '\n')
     let ret = SnpEntry chrom pos ref alt
     -- trace (show ret) $ return ()
-    return $ SnpEntry chrom pos ref alt
+    return ret
 
-printFreqSum :: Text -> FreqSumRow -> Text
-printFreqSum outChrom (FreqSumRow _ pos ref alt calls) =
+showFreqSum :: Text -> FreqSumRow -> Text
+showFreqSum outChrom (FreqSumRow _ pos ref alt calls) =
     format (s%"\t"%d%"\t"%s%"\t"%s%"\t"%s) outChrom pos (T.singleton ref) (T.singleton alt) callsStr
   where
     callsStr = (T.intercalate "\t" . map (format d)) calls
@@ -325,7 +366,7 @@ printEigenStrat outChrom snpOutHandle = for cat $ \(FreqSumRow _ pos ref alt cal
     yield . T.concat . map (format d . toEigenStratNum) $ calls
   where
     toEigenStratNum c = case c of
-        0 -> 2
+        0 -> 2 :: Int
         1 -> 1
         2 -> 0
         -1 -> 9
