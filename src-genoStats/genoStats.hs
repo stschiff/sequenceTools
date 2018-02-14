14c55065a5fc796ad25a47d207cc3577f67b8418
Stephan Schiffels <schiffels_schiffels@mac.com>
2018-02-15 00:25:22 +0100

    compiles

--- src-genoStats/genoStats.hs
+++ src-genoStats/genoStats.hs
@@ -1,29 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified SequenceFormats.Eigenstrat as E
-import SequenceFormats.FreqSum (parseFreqSum, FreqSumHeader(..), FreqSumEntry(..), 
-    streamEigenstratGeno, streamEigenstratSnp)
+import SequenceFormats.FreqSum (parseFreqSum, FreqSumHeader(..), FreqSumEntry(..))
+import SequenceFormats.Eigenstrat (streamEigenstratGeno, streamEigenstratSnp)
 
-import Control.Error (Script, runScript, scriptIO)
-import Data.Text (Text, pack, unpack)
+import Data.Text (Text, pack)
 import Data.Version (showVersion)
 import Paths_sequenceTools (version)
 import Pipes (for, Producer, runEffect, (>->), yield)
 import qualified Pipes.Prelude as P
-import Pipes.Text.IO (fromHandle)
 import Prelude hiding (FilePath)
 import System.IO (IOMode(..), openFile, stdin)
-import Turtle hiding (stdin)
+import Turtle hiding (stdin, x)
 
-data ProgOpt = ProgOpt {
-    optInput :: InputOption,
-    optVersion :: Bool
-}
+data ProgOpt = ProgOpt InputOption Bool
 
 data InputOption = FreqsumInput (Maybe FilePath) | EigenstratInput FilePath FilePath FilePath
 
-data InputEntry = InputEntry Int [Genotype]
-data Genotype = HomRef | HomAlt | Het | Missing
+data InputEntry = InputEntry Int [Genotype] deriving (Show)
+data Genotype = HomRef | HomAlt | Het | Missing deriving (Show)
 
 main :: IO ()
 main = options descString argParser >>= runWithOpts
@@ -38,11 +33,13 @@ argParser = ProgOpt <$> parseInputOpt <*> parseVersionOpt
     parseVersionOpt = switch "version" 'v' "Print the version and exit"
 
 parseFreqsumInput :: Parser InputOption
-parseFreqsumInput = do
-    p <- optText "freqsum" 'f' "a freqsum file to read as input. Use - to read from stdin"
-    if p == "-"
-    then return $ FreqsumInput Nothing
-    else return . FreqsumInput . Just . fromText $ p
+parseFreqsumInput =
+    process <$> optText "freqsum" 'f' "a freqsum file to read as input. Use - to read from stdin"
+  where
+    process p =
+        if p == "-"
+        then FreqsumInput Nothing
+        else FreqsumInput . Just . fromText $ p
 
 parseEigenstratInput :: Parser InputOption
 parseEigenstratInput = EigenstratInput <$> parseSnpFile <*> parseIndFile <*> parseGenoFile
@@ -70,17 +67,18 @@ runWithFreqSum :: Maybe FilePath -> IO ([Text], Producer InputEntry IO ())
 runWithFreqSum fsFile = do
     handle <- case fsFile of
         Nothing -> return stdin
-        Just f -> openFile (repr f) ReadMode
+        Just fn -> openFile (repr fn) ReadMode
     (FreqSumHeader namesStr nrHaps, fsProd) <- parseFreqSum handle
     let names = map pack namesStr
     let prod = for fsProd $ \(FreqSumEntry chrom _ _ _ counts) -> do
             let genotypes = do
-                    (count, nrHap) <- zip counts nrHaps
-                    case count of
+                    (count', nrHap) <- zip counts nrHaps
+                    case count' of
                         0 -> return HomRef
                         x | x == nrHap -> return HomAlt
                           | x > 0 && x < nrHap -> return Het
                           | x < 0 -> return Missing
+                        _ -> error "should not happen"
             yield $ InputEntry chrom genotypes
     return (names, prod)
 
@@ -88,8 +86,8 @@ runWithEigenstrat :: FilePath -> FilePath -> FilePath -> IO ([Text], Producer In
 runWithEigenstrat genoFile snpFile indFile = do
     indEntries <- E.readEigenstratInd (repr indFile)
     let names = [name | E.EigenstratIndEntry name _ _ <- indEntries]
-    genoHandle <- openFile genoFile ReadMode
-    snpHandle <- openFile snpFile ReadMode
+    genoHandle <- openFile (repr genoFile) ReadMode
+    snpHandle <- openFile (repr snpFile) ReadMode
     let genoProd = streamEigenstratGeno genoHandle
         snpProd = streamEigenstratSnp snpHandle
         zippedProducer = P.zip snpProd genoProd
@@ -101,7 +99,7 @@ runWithEigenstrat genoFile snpFile indFile = do
                         E.HomAlt -> return HomAlt
                         E.Het -> return Het
                         E.Missing -> return Missing
-            yield $ InputEntry chrom genotypes
+            yield $ InputEntry (read . repr $ chrom) genotypes
     return (names, prod)
 
         
